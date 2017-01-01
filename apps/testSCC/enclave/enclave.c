f4463cd5c522967d246feab4cc45e3c7ca930a54
Rohit Sinha <rsinha@berkeley.edu>
2017-01-01 13:00:35 -0800

    minor mod

--- apps/testSCC/enclave/enclave.c
+++ apps/testSCC/enclave/enclave.c
@@ -7,15 +7,16 @@
 
 uint64_t enclave_test()
 {
-    blob_t blob;
+    blob_t blob1, blob2, blob3;
     scc_ctx_t *ctx = _moat_scc_create();
-    blob.x = 42;
-    _moat_scc_send(ctx, &blob, sizeof(blob));
-    _moat_scc_recv(ctx, &blob, sizeof(blob));
+    blob1.x = 42;
+    _moat_scc_send(ctx, &blob1, sizeof(blob1));
+    _moat_scc_recv(ctx, &blob2, sizeof(blob2));
+    _moat_print_debug("blob contains %" PRIu64 "\n", blob2.x);
     //trusted increment
-    blob.x += 1;
-    _moat_scc_send(ctx, &blob, sizeof(blob));        
-    _moat_scc_recv(ctx, &blob, sizeof(blob));
-    _moat_print_debug("test returned %" PRIu64 "\n", blob.x);
+    blob2.x += 1;
+    _moat_scc_send(ctx, &blob2, sizeof(blob2));
+    _moat_scc_recv(ctx, &blob3, sizeof(blob3));
+    _moat_print_debug("blob contains %" PRIu64 "\n", blob3.x);
     return 0;
 }
