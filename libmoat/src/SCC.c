64bdc6a986c68d533c01f43b832d71f0c60bca53
Rohit Sinha <rsinha@berkeley.edu>
2017-02-01 18:22:28 -0800

    implemented some additional checks in send and recv

--- libmoat/src/SCC.c
+++ libmoat/src/SCC.c
@@ -1,3 +1,6 @@
+//NIST guidelines: http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+//TLS 1.3 Spec: https://tlswg.github.io/tls13-spec/
+
 #include <stddef.h> 
 
 #include <assert.h>
@@ -11,7 +14,16 @@
 #include "attestation/local/error_codes.h"
 #include "attestation/local/EnclaveMessageExchange.h"
 
-//NIST guidelines: http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+typedef enum { RESET = 0,
+               TEARDOWN = 1,
+               APPLICATION_DATA = 2
+             } libmoat_ciphertext_type_t;
+
+typedef struct
+{
+    size_t type;
+    size_t length;
+} libmoat_ciphertext_header_t;
 
 scc_ctx_t *_moat_scc_create(bool is_server, sgx_measurement_t *measurement)
 {
@@ -54,36 +66,40 @@ size_t _moat_scc_send(scc_ctx_t *ctx, void *buf, size_t len)
     if (session_info->local_counter > (1 << 24)) { return 1; }
 
     //allocate memory for ciphertext
-    size_t dst_len = SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE + len;
+    size_t dst_len = sizeof(libmoat_ciphertext_header_t) + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE + len;
     //look for overflows
     assert(dst_len > len);
-    uint8_t *dst_buf = (uint8_t *) malloc(dst_len);
-    assert (dst_buf != NULL);
+    uint8_t *ciphertext = (uint8_t *) malloc(dst_len);
+    assert (ciphertext != NULL);
 
-    //status = sgx_read_rand((unsigned char *) dst_buf + 0, SGX_AESGCM_IV_SIZE);
-    //assert(status == SGX_SUCCESS);
+    ((libmoat_ciphertext_header_t *) ciphertext)->type = APPLICATION_DATA;
+    ((libmoat_ciphertext_header_t *) ciphertext)->length = SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE + len;
+
+    uint8_t *payload = ciphertext + sizeof(libmoat_ciphertext_header_t);
+
     uint32_t nonce = session_info->local_counter;
-    memcpy(dst_buf + 0, &nonce, sizeof(nonce));
-    memset(dst_buf + sizeof(nonce), 0, SGX_AESGCM_IV_SIZE - sizeof(nonce));
+    //nonce is 64 bits of 0 followed by the message sequence number
+    memcpy(payload + 0, &nonce, sizeof(nonce));
+    memset(payload + sizeof(nonce), 0, SGX_AESGCM_IV_SIZE - sizeof(nonce));
 
     /* ciphertext: IV || MAC || encrypted */
     status = sgx_rijndael128GCM_encrypt((const sgx_aes_gcm_128bit_key_t *) &(session_info->AEK),
                                         buf, /* input */
                                         len, /* input length */
-                                        dst_buf + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE, /* out */
-                                        dst_buf + 0,
-                                        SGX_AESGCM_IV_SIZE,
-                                        NULL,
-                                        0,
-                                        (sgx_aes_gcm_128bit_tag_t *) (dst_buf + SGX_AESGCM_IV_SIZE));
+                                        payload + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE, /* out */
+                                        payload + 0, /* IV */
+                                        SGX_AESGCM_IV_SIZE, /* 12 bytes of IV */
+                                        NULL, /* additional data */
+                                        0, /* zero bytes of additional data */
+                                        (sgx_aes_gcm_128bit_tag_t *) (payload + SGX_AESGCM_IV_SIZE)); /* mac */
     assert(status == SGX_SUCCESS);
 
     //so we don't reuse IVs
     session_info->local_counter = session_info->local_counter + 2;
 
-    status = send_msg_ocall(&retstatus, dst_buf, dst_len, ctx->session_id);
+    status = send_msg_ocall(&retstatus, ciphertext, dst_len, ctx->session_id);
     assert(status == SGX_SUCCESS && retstatus == 0);
-    free(dst_buf);
+    free(ciphertext);
 }
 
 size_t _moat_scc_recv(scc_ctx_t *ctx, void *buf, size_t len)
@@ -91,7 +107,7 @@ size_t _moat_scc_recv(scc_ctx_t *ctx, void *buf, size_t len)
     sgx_status_t status;
     uint32_t retstatus;
     size_t actual_len;
-    size_t max_len = SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE + len;
+    size_t max_len = sizeof(libmoat_ciphertext_header_t) + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE + len;
 
     uint8_t *ciphertext = (uint8_t *) malloc(max_len);
     assert(ciphertext != NULL);
@@ -100,28 +116,33 @@ size_t _moat_scc_recv(scc_ctx_t *ctx, void *buf, size_t len)
     assert(status == SGX_SUCCESS && retstatus == 0);
     assert (actual_len <= max_len); //although the caller cannot write past len, it may set actual to be an arbitrary value
 
+    if (((libmoat_ciphertext_header_t *) ciphertext)->type != APPLICATION_DATA) { return 0; } //no bytes
+    if (((libmoat_ciphertext_header_t *) ciphertext)->length != (SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE + len)) { return 0; }
+
+    uint8_t *payload = ciphertext + sizeof(libmoat_ciphertext_header_t);
+
     dh_session_t *session_info = get_session_info(ctx->session_id);
     assert(session_info != NULL);
 
-    /* ciphertext: IV || MAC || encrypted */
+    /* ciphertext: header || IV || MAC || encrypted */
     status = sgx_rijndael128GCM_decrypt((const sgx_aes_gcm_128bit_key_t *) &(session_info->AEK),
-                                        ciphertext + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE,
-                                        actual_len - (SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE),
+                                        payload + SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE,
+                                        ((libmoat_ciphertext_header_t *) ciphertext)->length - (SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE),
                                         buf,
-                                        ciphertext,
+                                        payload,
                                         SGX_AESGCM_IV_SIZE,
                                         NULL,
                                         0,
-                                        (const sgx_aes_gcm_128bit_tag_t *) (ciphertext + SGX_AESGCM_IV_SIZE));
+                                        (const sgx_aes_gcm_128bit_tag_t *) (payload + SGX_AESGCM_IV_SIZE));
     assert(status == SGX_SUCCESS);
 
     uint32_t nonce;
-    memcpy(&nonce, ciphertext, sizeof(nonce));
+    memcpy(&nonce, payload, sizeof(nonce));
     assert(nonce > session_info->remote_counter); //to prevent replay attacks
     session_info->remote_counter = nonce;
 
     free(ciphertext);
-    return actual_len - (SGX_AESGCM_IV_SIZE + SGX_AESGCM_MAC_SIZE);
+    return len;
 }
 
 void _moat_scc_destroy(scc_ctx_t *ctx)
