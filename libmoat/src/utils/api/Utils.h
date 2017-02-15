bb158d5fd7cc7499ea2348b1c4b50ec863463c55
Rohit Sinha <rsinha@berkeley.edu>
2017-02-14 20:56:23 -0800

    documentation

--- libmoat/src/utils/api/Utils.h
+++ libmoat/src/utils/api/Utils.h
@@ -5,6 +5,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/***************************************************
+                MATH UTILITIES
+ ***************************************************/
+
+size_t min(size_t a, size_t b);
+
+/***************************************************
+            LINKED LIST UTILITIES
+ ***************************************************/
+
 typedef struct
 {
     void *head; //type ll_node_t
@@ -15,9 +25,6 @@ typedef struct
     void *next_node; //type ll_node_t
 } ll_iterator_t;
 
-
-size_t min(size_t a, size_t b);
-
 //returns the size of the linked list
 uint32_t list_size(ll_t *list);
 //inserts value at the tail of the linked list
@@ -26,9 +33,13 @@ void insert_value(ll_t *list, void *value);
 bool delete_value(ll_t *list, void *value);
 //finds first node in the linked list which satisfies pred
 void *find_value(ll_t *list, bool (*pred)(void *));
+//iterator to invoke has_next and get_next
 ll_iterator_t *create_iterator(ll_t *list);
+//free resources used by the iterator
 void destroy_iterator(ll_iterator_t *iter);
+//does the list have a next item not yet consumed by iter?
 bool has_next(ll_iterator_t *iter);
+//return the next item in the list
 void *get_next(ll_iterator_t *iter);
 
 #endif
