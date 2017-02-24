88a3dbf658b9d6d666053551b00e09709b0cdbb1
Rohit Sinha <rsinha@berkeley.edu>
2017-02-23 21:35:01 -0800

    using SHA-256 for freshness checks

--- libmoat/src/storage/BlockStorage.c
+++ libmoat/src/storage/BlockStorage.c
@@ -31,7 +31,7 @@ typedef struct
  ***************************************************/
 
 static uint32_t g_local_counter = 0;
-static sgx_aes_gcm_128bit_tag_t macs[NUM_BLOCKS];
+static sgx_sha256_hash_t latest_hash[NUM_BLOCKS];
 
 /***************************************************
                 PRIVATE METHODS
@@ -61,7 +61,10 @@ size_t read_access(size_t addr, block_t data, sgx_aes_gcm_128bit_key_t *key)
     uint8_t *payload = ciphertext + sizeof(fs_ciphertext_header_t);
     
     //preventing rollback attacks
-    assert(memcmp(&(macs[addr - 1]), payload + SGX_AESGCM_IV_SIZE, SGX_AESGCM_MAC_SIZE) == 0);
+    sgx_sha256_hash_t computed_hash;
+    status = sgx_sha256_msg(ciphertext, len, &computed_hash);
+    assert(status == SGX_SUCCESS);
+    assert(memcmp(&(latest_hash[addr - 1]), &computed_hash, sizeof(computed_hash)) == 0);
 
     /* ciphertext: header || IV || MAC || encrypted */
     status = sgx_rijndael128GCM_decrypt((const sgx_aes_gcm_128bit_key_t *) key, //key
@@ -114,7 +117,8 @@ size_t write_access(size_t addr, block_t data, sgx_aes_gcm_128bit_key_t *key)
     assert(status == SGX_SUCCESS);
     
     //saving MAC for checking freshness of ciphertexts
-    memcpy(&(macs[addr - 1]), payload + SGX_AESGCM_IV_SIZE, SGX_AESGCM_MAC_SIZE);
+    status = sgx_sha256_msg(ciphertext, len, &(latest_hash[addr - 1]));
+    assert(status == SGX_SUCCESS);
 
     //so we don't reuse IVs
     g_local_counter = g_local_counter + 1;
