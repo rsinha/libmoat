a772addd007e55f4ecd70437970eeb01560c52cf
Vinay Koshy <vkoshy@berkeley.edu>
2017-07-17 14:17:05 -0700

    separated enclaves into separate files

--- apps/passwordChecker/password/enclave/enclave.c
+++ apps/passwordChecker/password/enclave/enclave.c
@@ -61,7 +61,7 @@ static void pwrecord_encode(pwrecord *pwr, uint8_t* out) {
 }
 
 static uint32_t pwrecord_decode(pwrecord *pwr, const uint8_t *buf) {
-   _moat_print_debug("here");
+   _moat_print_debug("here\n");
    pwr->version = read32_be(buf);
    pwr->iters = read32_be(buf+4);
    memcpy(pwr->salt, buf+8, sizeof pwr->salt);
@@ -86,34 +86,83 @@ static uint32_t pwrecord_check_pass(pwrecord *pwr, const uint8_t *password,
     return 0;
 
 }
-uint32_t pw_setup(const uint8_t* password, uint32_t pwlen, uint8_t* blob, uint32_t bloblen_in) {
-	pwrecord pwr = { 0 };
+uint64_t pw_setup(uint8_t* password, uint32_t passlen, uint8_t* user, 
+	uint32_t userlen) {
+        pwrecord pwr = { 0 };
         uint32_t err = pwrecord_new(&pwr);
 	if (err == 0) {
             return err;
 	}
-        pwrecord_fill_hash(&pwr, password, pwlen);
+        uint8_t blob[56];
+        pwrecord_fill_hash(&pwr, password, 5);
         pwrecord_encode(&pwr, blob);
+        fs_handle_t *fd = _moat_fs_open(user);
+        _moat_fs_write(fd, 0, &blob, sizeof(blob));
+        _moat_print_debug("set up info recieved\n");
         return 1;
 }
-uint32_t pw_check(const uint8_t *password, uint32_t pwlen, uint8_t *orig,
-	uint32_t origlen) {
+uint64_t pw_check() {
+    uint8_t password[5];
+    uint8_t user[5];
+    //ideally some authority (e.g. CA) will tell us this
+    sgx_measurement_t measurement = { .m = { 0x55,0xCF,0x87,0x7E,0xFF,0x2F,0xE4,0x09,
+                                             0x6F,0x16,0x5B,0xC1,0x0D,0x06,0xCB,0x7A,
+                                             0xAF,0x49,0xEF,0x4B,0xCE,0xBD,0xEA,0x90,
+                                             0xD6,0x28,0x98,0xBC,0xBC,0x8F,0x50,0x36 } };
+    scc_attributes_t attr = { .record_size = 128, .side_channel_protection = 0 };
+
+    scc_handle_t *handle = _moat_scc_create(true, &measurement, &attr);
+    _moat_scc_recv(handle, user, 5);
+    _moat_scc_recv(handle, password, 5);
+    int api_result = _moat_scc_destroy(handle); assert(api_result == 0);
     pwrecord pwr = { 0 };
     uint32_t err;
     if (err == 0) {
         return err;
     }
-    pwrecord_decode(&pwr, orig);
-    err = pwrecord_check_pass(&pwr, password, pwlen);
+    uint8_t orig[56];
+    fs_handle_t *fd = _moat_fs_open(user);
+    _moat_fs_read(fd, 0, orig, 56);
+    pwrecord_decode(&pwr, password);
+    err = pwrecord_check_pass(&pwr, password, sizeof password);
     pwrecord_clean(&pwr);
+    _moat_print_debug("check info recieved: %d\n", err);
     return err;
 
 }
+
+
+uint64_t pw_starter() {
+     _moat_print_debug("Entered starter\n");
+     sgx_measurement_t measurement = { .m = { 0x6A,0xD5,0x51,0xD6,0x40,0x9F,0xA1,0x9B,
+                                             0x96,0x2A,0x5B,0x5B,0xCB,0x2E,0xD4,0x08,
+                                             0x11,0xB8,0x86,0x5A,0x77,0x2A,0x53,0xEA,
+                                             0x7D,0x56,0x45,0x10,0x51,0xD4,0x9C,0x52 } };
+    scc_attributes_t attr = { .record_size = 128, .side_channel_protection = 0 };
+    scc_handle_t *handle = _moat_scc_create(true, &measurement, &attr);
+    _moat_print_debug("Created SCC\n");
+    uint8_t user[5];
+    uint8_t password[5];
+    uint8_t user2[5];
+    uint8_t pass2[5];
+    uint8_t user3[5];
+    uint8_t pass3[5];
+    _moat_scc_recv(handle, user, 5);
+    _moat_scc_recv(handle, password, 5);
+    _moat_scc_recv(handle, user2, 5);
+    _moat_scc_recv(handle, pass2, 5);
+    _moat_scc_recv(handle, user3, 5);
+    _moat_scc_recv(handle, pass3, 5);
+    _moat_print_debug("%s\n", password);
+    int api_result = _moat_scc_destroy(handle); assert(api_result == 0);
+}
+
+
 uint64_t enclave_test()
 {
     _moat_debug_module_init();
     _moat_scc_module_init();
-
+    _moat_fs_module_init();
     //ideally some authority (e.g. CA) will tell us this
     sgx_measurement_t measurement = { .m = { 0x6A,0xD5,0x51,0xD6,0x40,0x9F,0xA1,0x9B,
                                              0x96,0x2A,0x5B,0x5B,0xCB,0x2E,0xD4,0x08,
--- apps/passwordChecker/password/host/host_server.cpp
+++ apps/passwordChecker/password/host/host_server.cpp
@@ -15,97 +15,23 @@
 using namespace std;
 /* Global EID shared by multiple threads */
 sgx_enclave_id_t global_eid = 0;
-
 int enclave_computation(uint8_t control)
 {
-  uint32_t pwerr;
+  uint64_t pwerr;
 
   /* Setup enclave */
   sgx_status_t ret;
   sgx_launch_token_t token = { 0 };
-     
   int token_updated = 0;
-
+  sgx_launch_token_t token2 = { 0 };
   ret = sgx_create_enclave("enclave.signed.so", SGX_DEBUG_FLAG, &token, &token_updated, &global_eid, NULL);
   if (ret != SGX_SUCCESS)
   {
     printf("sgx_create_enclave failed: %#x\n", ret);
     return 1;
   }
-  printf("created enclave...\n");
-  string username = "Vinay";
-  string password = "alsovinay";
-  string user2 = "ABC";
-  string pass2 = "DEF";
-  string passwrong = "JKL";
-  string alt_pass = "GHI";
-  uint8_t to_write[56];
-  uint8_t to_write2[56];
-  uint8_t to_write3[56];
-  uint8_t to_write4[56];
-  uint8_t to_read2[56];
-  ret = pw_setup(global_eid, &pwerr, (const uint8_t*) password.c_str(), password.length(), to_write, 56);
-  assert(ret == SGX_SUCCESS && pwerr == 1);
-  FILE* write_to = fopen(username.c_str(), "w");
-  fwrite(to_write, 56, 1, write_to);
-  fclose(write_to);
-  printf("file written\n");
-  uint8_t to_read[56];
-  FILE* read_from = fopen(username.c_str(), "r");
-  fread(to_read, 56, 1, read_from);
-  printf("file read\n");
-  printf("entering enclave\n");
-  std::cout.flush();
-  ret = pw_check(global_eid, &pwerr, (const uint8_t*) password.c_str(), password.length(), to_read, 56);
-  std::cout.flush();
-  std::cerr.flush();
-  //printf("pw_check+ took %ums\n", GetTickCount() - time);
-  //Check if it verifies the correct password.
-  assert(pwerr == 1);
-  //Check that it rejects incorrect password
-  ret = pw_check(global_eid, &pwerr, (const uint8_t*) pass2.c_str(), pass2.length(), to_read, 56);
-  assert(pwerr == 0);
-  //Change existing password
-  ret = sgx_destroy_enclave(global_eid);
-  sgx_create_enclave("enclave.signed.so", SGX_DEBUG_FLAG, &token, &token_updated, &global_eid, NULL);
-  pw_setup(global_eid, &pwerr, (const uint8_t*) pass2.c_str(), pass2.length(), to_write2, 56);
-  assert(pwerr == 1);
-  FILE* write_to2 = fopen(username.c_str(), "w");
-  fwrite(to_write2, 56, 1, write_to2);
-  fclose(write_to2);
-  FILE* read_from2 = fopen(username.c_str(), "r");
-  fread(to_read2, 56, 1, read_from2);
-  pw_check(global_eid, &pwerr, (const uint8_t*) password.c_str(), password.length(), to_read2, 56);
-  assert(pwerr == 0);
-  pw_check(global_eid, &pwerr, (const uint8_t*) pass2.c_str(), pass2.length(), to_read2, 56);
-  assert(pwerr == 1);
-  //Ensure its possible to have concurrent accounts
-  pw_setup(global_eid, &pwerr, (const uint8_t*) alt_pass.c_str(), alt_pass.length(), to_write3, 56);
-  assert(pwerr == 1);
-  FILE* write_to3 = fopen(user2.c_str(), "w");
-  fwrite(to_write3, 56, 1, write_to3);
-  fclose(write_to3);
-  FILE* read_from3 = fopen(user2.c_str(), "r");
-  uint8_t to_read3[56];
-  fread(to_read3, 56, 1, read_from3);
-  pw_check(global_eid, &pwerr, (const uint8_t*) alt_pass.c_str(), alt_pass.length(), to_read3, 56);
-  assert(pwerr == 1);
-  pw_check(global_eid, &pwerr, (const uint8_t*) pass2.c_str(), pass2.length(), to_read3, 56);
-  assert(pwerr == 0);
-  pw_check(global_eid, &pwerr, (const uint8_t*) alt_pass.c_str(), alt_pass.length(), to_read2, 56);
-  assert(pwerr == 0);
-  pw_check(global_eid, &pwerr, (const uint8_t*) pass2.c_str(), pass2.length(), to_read2, 56);
-  assert(pwerr ==1);
-  std::cout.flush();
-  sgx_destroy_enclave(global_eid);
-  if (ret != SGX_SUCCESS)
-  {
-    printf("fail 2\n");
-    return 0;
-  }
-  printf("success\n");
-  std::cout.flush();
-  return 1;
+ pw_starter(global_eid, &pwerr);
+ printf("done??\n");
 }
 
 
