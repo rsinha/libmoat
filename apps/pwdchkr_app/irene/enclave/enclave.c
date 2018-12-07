4aa30bb151b1fbcc7e6fc3ea5d0f81f440f51795
Rohit Sinha <rsinha@berkeley.edu>
2018-12-07 13:27:38 -0800

    writing irene's input to file

--- apps/pwdchkr_app/irene/enclave/enclave.c
+++ apps/pwdchkr_app/irene/enclave/enclave.c
@@ -15,9 +15,6 @@ uint64_t enclave_test(uint8_t *buf, size_t size)
     _moat_fs_module_init();
 
     _moat_print_debug("enclave: buf: %p, size: %zu\n", buf, size);
-    //uint8_t *internal_buf = (uint8_t *) malloc(size);
-    //_moat_print_debug("malloc \n");
-    //assert(!internal_buf);
 
     LuciditeeGuessApp__Secret *secret;
     secret = luciditee_guess_app__secret__unpack(NULL, size, buf);
@@ -27,6 +24,26 @@ uint64_t enclave_test(uint8_t *buf, size_t size)
         PRIu64 ", with max guesses %" PRIu64 "\n", secret->value, secret->guesses);
 
     luciditee_guess_app__secret__free_unpacked(secret, NULL);
+
+    //first copy the proto buffer internally
+    uint8_t *internal_buf = (uint8_t *) malloc(size);
+    _moat_print_debug("malloc \n");
+    assert(internal_buf != NULL);
+    memcpy(internal_buf, buf, size);
+
+    //create a file out of this protobuf message
+    //first create the key
+    sgx_aes_gcm_128bit_key_t fs_encr_key;
+    memset(&fs_encr_key, 0, sizeof(fs_encr_key)); //TODO: this is zeroed out now
+
+    //open the file
+    int64_t fd = _moat_fs_open("irene_input", O_WRONLY | O_CREAT, &fs_encr_key);
+    assert(fd != -1);
+    int64_t api_result = _moat_fs_write(fd, buf, size);
+    assert(api_result == size);
+    api_result = _moat_fs_save(fd);
+    assert(api_result == 0);
+
     return 0;
 }
 
