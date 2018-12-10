80ba4696b1d5a792a7edd113cc079c8bb0fa8777
Rohit Sinha <rsinha@berkeley.edu>
2018-12-10 09:06:56 -0800

    fix

--- apps/pwdchkr_app/compute/enclave/enclave.c
+++ apps/pwdchkr_app/compute/enclave/enclave.c
@@ -61,50 +61,70 @@ uint64_t enclave_transition()
     _moat_debug_module_init();
     _moat_fs_module_init();
 
-    sgx_aes_gcm_128bit_key_t irene_encr_key, sherlock_encr_key;
-    memset(&irene_encr_key, 0, sizeof(irene_encr_key));
+    sgx_aes_gcm_128bit_key_t state_encr_key, sherlock_encr_key;
+    memset(&state_encr_key, 0, sizeof(state_encr_key));
     memset(&sherlock_encr_key, 0, sizeof(sherlock_encr_key));
 
-    int64_t irene_fd = _moat_fs_open("irene_input", O_RDONLY, &irene_encr_key);
-    assert(irene_fd != -1);
+    int64_t state_fd = _moat_fs_open("pwdchkr_state", O_RDWR, &state_encr_key);
+    assert(state_fd != -1);
     int64_t sherlock_fd = _moat_fs_open("sherlock_input", O_RDONLY, &sherlock_encr_key);
     assert(sherlock_fd != -1);
 
-    size_t irene_buf_len = (size_t) get_file_size(irene_fd);
-    _moat_print_debug("Irene's input has size %" PRIu64 "\n", irene_buf_len);
+    size_t state_buf_len = (size_t) get_file_size(state_fd);
+    //_moat_print_debug("Irene's input has size %" PRIu64 "\n", state_buf_len);
     size_t sherlock_buf_len = (size_t) get_file_size(sherlock_fd);
-    _moat_print_debug("Sherlock's input has size %" PRIu64 "\n", sherlock_buf_len);
+    //_moat_print_debug("Sherlock's input has size %" PRIu64 "\n", sherlock_buf_len);
 
-    uint8_t *irene_buf = (uint8_t *) malloc(irene_buf_len);
-    uint8_t *sherlock_buf = (uint8_t *) malloc(sherlock_buf_len);
+    uint8_t *state_buf = (uint8_t *) malloc(state_buf_len); assert(state_buf != NULL);
+    uint8_t *sherlock_buf = (uint8_t *) malloc(sherlock_buf_len); assert(sherlock_buf != NULL);
 
-    int64_t api_result = _moat_fs_read(irene_fd, irene_buf, irene_buf_len);
-    assert(api_result == irene_buf_len);
+    int64_t api_result = _moat_fs_read(state_fd, state_buf, state_buf_len);
+    assert(api_result == state_buf_len);
     api_result = _moat_fs_read(sherlock_fd, sherlock_buf, sherlock_buf_len);
     assert(api_result == sherlock_buf_len);
-
     
-    
-    LuciditeeGuessApp__Secret *secret;
-    secret = luciditee_guess_app__secret__unpack(NULL, irene_buf_len, irene_buf);
-    assert(secret != NULL);
-    _moat_print_debug("parsing proto from enclave...got secret value %" 
-        PRIu64 ", with max guesses %" PRIu64 "\n", secret->password, secret->guesses);
+    LuciditeeGuessApp__Secret *state;
+    state = luciditee_guess_app__secret__unpack(NULL, state_buf_len, state_buf);
+    assert(state != NULL);
+    _moat_print_debug("parsing state within enclave...got secret password %" 
+        PRIu64 ", with remaining guesses %" PRIu64 "\n", state->password, state->guesses);
 
     LuciditeeGuessApp__Attempt *attempt;
     attempt = luciditee_guess_app__attempt__unpack(NULL, sherlock_buf_len, sherlock_buf);
     assert(attempt != NULL);
-    _moat_print_debug("parsing proto from enclave...got attempt value %" PRIu64 "\n", attempt->guess);
+    _moat_print_debug("parsing Sherlock's input from enclave...got guess value %" PRIu64 "\n", attempt->guess);
 
+    assert (state->guesses > 0); //should have been caught in policy checking phase
 
-
-    if (attempt->guess == secret->password) {
-
+    char *output;
+    if (attempt->guess == state->password) {
+        output = "success"; //write success message
+        state->guesses = 0; //set guesses to 0 to terminate the game
+    } else {
+        output = "failure"; //write failure message
+        state->guesses = state->guesses - 1; //decrement guesses by 1
     }
 
-    luciditee_guess_app__secret__free_unpacked(secret, NULL);
-    luciditee_guess_app__attempt__free_unpacked(attempt, NULL);
+    //serialize state to state_buf
+    assert(state_buf_len == luciditee_guess_app__secret__get_packed_size(state));
+    assert (luciditee_guess_app__secret__pack(state, state_buf) == state_buf_len);
+    //write to fd
+    api_result = _moat_fs_write(state_fd, state_buf, state_buf_len);
+    assert(api_result == state_buf_len);
+    api_result = _moat_fs_save(state_fd);
+    assert(api_result == 0);
 
+    sgx_aes_gcm_128bit_key_t output_encr_key;
+    memset(&output_encr_key, 0, sizeof(output_encr_key));
+    int64_t output_fd = _moat_fs_open("pwdchkr_output", O_RDWR | O_CREAT, &output_encr_key);
+    assert(output_fd != -1);
+    api_result = _moat_fs_write(output_fd, output, strlen(output) + 1);
+    assert(api_result == strlen(output) + 1);
+    api_result = _moat_fs_save(output_fd);
+    assert(api_result == 0);
+
+    luciditee_guess_app__secret__free_unpacked(state, NULL);
+    luciditee_guess_app__attempt__free_unpacked(attempt, NULL);
 
     return 0;
 }
