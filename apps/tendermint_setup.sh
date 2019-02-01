962813632edc93be7d1723fae067524482a94c2b
Rohit Sinha <sinharo@gmail.com>
2019-02-01 19:29:20 +0000

    tendermint scripts

--- apps/tendermint_setup.sh
+++ apps/tendermint_setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-echo "Installing Tendermint...."
-mkdir -p $GOPATH/src/github.com/tendermint
-cd $GOPATH/src/github.com/tendermint
-git clone https://github.com/tendermint/tendermint.git
-cd tendermint
-
-make get_tools
-make get_vendor_deps
-
-make install
-
 echo "Running Tendermint..."
 tendermint init
 nohup tendermint node --proxy_app=kvstore &
